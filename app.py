49973bcbb417f6a6d937d6b4ef7fc141a99df614
MWIKALI MARTIN KIOKO <149610202+33Martin22@users.noreply.github.com>
2025-12-06 23:33:43 +0300

    Update Gemini model to version 1.5-flash

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ GEMINI_API_KEY = "AIzaSyBy2ToDOl5BAfyRLLQNVXQvbSf0SY-mSEw"
 GEMINI_API_KEY = st.secrets["GEMINI_API_KEY"]
 
 genai.configure(api_key=GEMINI_API_KEY)
-gemini_model = genai.GenerativeModel("gemini-3-pro-preview")
+gemini_model = genai.GenerativeModel("gemini-1.5-flash")
 
 # --------------------------------
 # 🧾 FAQ Dataset
